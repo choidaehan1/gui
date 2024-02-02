b109de725530d2326e2d2f12898cc9e7d85fa095
[choidaehan1] <[cdh6805@naver.com]>
2024-02-02 16:15:39 +0900

    Gui

---
+++ src/swing/winEmpdddd.java
@@ -0,0 +1,220 @@
+package swing;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class winEmpdddd extends JFrame {
+
+    JTextField tf1 = new JTextField(5);
+    JTextField tf2 = new JTextField(5);
+    JTextField tf3 = new JTextField(5);
+    JTextField tf4 = new JTextField(5);
+    JTextField tf5 = new JTextField(5);
+    JTextField tf6 = new JTextField(5);
+    JTextField tf7 = new JTextField(5);
+    JTextField tf8 = new JTextField(5);
+
+    JButton bt1 = new JButton("전체 내용");
+    JButton bt2 = new JButton("입력");
+    JButton bt3 = new JButton("이름 검색");
+    JButton bt4 = new JButton("수정");
+    JButton bt5 = new JButton("삭제");
+    JTextArea ta = new JTextArea(30, 35);
+    Connection conn;
+    Statement stmt;
+
+    public winEmpdddd() {
+        String url = "jdbc:mysql://localhost:3306/firm";
+        String id = "root";
+        String pass = "mysql";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(url, id, pass);
+            stmt = conn.createStatement();
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        JLabel lb1 = new JLabel("사번:");
+        JLabel lb2 = new JLabel("이름:");
+        JLabel lb3 = new JLabel("직무:");
+        JLabel lb4 = new JLabel("매니저사번:");
+        JLabel lb5 = new JLabel("입사년도:");
+        JLabel lb6 = new JLabel("월급:");
+        JLabel lb7 = new JLabel("보너스:");
+        JLabel lb8 = new JLabel("경력:");
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container con = this.getContentPane();
+        con.setLayout(new BorderLayout());
+        JPanel jp1 = new JPanel(new FlowLayout());
+        jp1.add(bt1);
+        jp1.add(bt2);
+        jp1.add(bt3);
+        jp1.add(bt4);
+        jp1.add(bt5);
+        con.add(jp1, BorderLayout.SOUTH);
+        JScrollPane scroll = new JScrollPane(ta);
+        JPanel jp2 = new JPanel(new FlowLayout());
+        jp2.add(scroll);
+        con.add(jp2, BorderLayout.CENTER);
+
+        JPanel jp3 = new JPanel(new FlowLayout());
+        con.add(jp3, BorderLayout.NORTH);
+        jp3.add(lb1);
+        jp3.add(tf1);
+        jp3.add(lb2);
+        jp3.add(tf2);
+        jp3.add(lb3);
+        jp3.add(tf3);
+        jp3.add(lb4);
+        jp3.add(tf4);
+        jp3.add(lb5);
+        jp3.add(tf5);
+        jp3.add(lb6);
+        jp3.add(tf6);
+        jp3.add(lb7);
+        jp3.add(tf7);
+        jp3.add(lb8);
+        jp3.add(tf8);
+        this.setTitle("EMP 관리");
+        this.setLocation(300, 400);
+        this.setSize(200, 300);
+        this.setVisible(true);
+
+        bt1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                select();
+            }
+        });
+
+        bt2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                insert();
+                clearTextField();
+                select();
+            }
+        });
+
+        bt3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                select2();
+            }
+        });
+
+        bt4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                updateOrDelete(true); // true: 업데이트
+            }
+
+			private void updateOrDelete(boolean b) {
+				// TODO Auto-generated method stub
+				
+			}
+        });
+
+        bt5.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                updateOrDelete(false); // false: 삭제
+            }
+
+			private void updateOrDelete(boolean b) {
+				// TODO Auto-generated method stub
+				
+			}
+        });
+    }
+
+    private void clearTextField() {
+        tf1.setText("");
+        tf2.setText("");
+        tf3.setText("");
+        tf4.setText("");
+        tf5.setText("");
+        tf6.setText("");
+        tf7.setText("");
+        tf8.setText("");
+    }
+
+    public void select() {
+        String sql = "select empno, ename,job,mgr,hiredate,sal,comm from emp";
+        try {
+            ResultSet rs = stmt.executeQuery(sql);
+            ta.setText("");
+            while (rs.next()) {
+                int empno = rs.getInt("empno");
+                String ename = rs.getString("ename");
+                String job = rs.getString("job");
+                int mgr = rs.getInt("mgr");
+                String hiredate = rs.getString("hiredate");
+                int sal = rs.getInt("sal");
+                int comm = rs.getInt("comm");
+                int deptno = rs.getInt("deptno");
+
+                String str = String.format("%d, %s,%s,%d,%s,%d,%d,%d/n ", empno, ename, job, mgr, hiredate, sal, comm,
+                        deptno);
+                ta.append(str);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void insert() {
+        String sql = "INSERT INTO emp VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, Integer.parseInt(tf1.getText())); // empno
+            pstmt.setString(2, tf2.getText()); // ename
+            pstmt.setString(3, tf3.getText()); // job
+            pstmt.setInt(4, Integer.parseInt(tf4.getText())); // mgr
+            pstmt.setString(5, tf5.getText()); // hiredate
+            pstmt.setInt(6, Integer.parseInt(tf6.getText())); // sal
+            pstmt.setInt(7, Integer.parseInt(tf7.getText())); // comm
+            pstmt.setInt(8, Integer.parseInt(tf8.getText())); // deptno
+
+            int res = pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void select2() {
+        String sql = "SELECT empno, ename,job,mgr,hiredate,sal,comm,deptno FROM emp WHERE ename = ?";
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, tf2.getText());
+
+            ResultSet rs = pstmt.executeQuery();
+            ta.setText("");
+            if (rs.next()) {
+                int empno = rs.getInt("empno");
+                String ename = rs.getString("ename");
+                String job = rs.getString("job");
+                int mgr = rs.getInt("mgr");
+                String hiredate = rs.getString("hiredate");
+                int sal = rs.getInt("sal");
+                int comm =
